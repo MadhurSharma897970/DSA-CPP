d1e9cbd918a96169c28a7dcb589404f97b3b7d8b
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-18 19:24:07 +0530

    Pattern printing

--- pattern_printing.cpp
+++ pattern_printing.cpp
@@ -13,3 +13,18 @@ int main(){
     }
 
 }
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char ch='A';
+        for(int j=0;j<n;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
