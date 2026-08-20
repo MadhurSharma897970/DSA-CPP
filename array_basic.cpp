662adc82c2b4d9a5048bd3e0d4fb368266ccb6db
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-20 21:27:09 +0530

    Min array element

--- array_basic.cpp
+++ array_basic.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     int size=sizeof(arr)/sizeof(int);
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
 int main(){
-    int arr[]={1,2,3,4,5};
-    int size=sizeof(arr)/sizeof(int);
+    int size;
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    int arr[size];
     for(int i=0;i<size;i++){
-        cout<<arr[i]<<" ";
+        cin>>arr[i];
     }
+    int min=arr[0];
+    for(int i=1;i<size;i++){
+        if(arr[i]<min) min=arr[i];
+    }
+    cout<<"The minimum number is: "<<min;
 }
 
