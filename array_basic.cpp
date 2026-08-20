89327c61546c6527737d36acfc32a01b729d3bc5
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-20 21:42:47 +0530

    Use min and max to find indexes

--- array_basic.cpp
+++ array_basic.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<climits>
 
 // int main(){
 //     int arr[]={1,2,3,4,5};
@@ -17,10 +18,28 @@ int main(){
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
-    int min=arr[0];
-    for(int i=1;i<size;i++){
-        if(arr[i]<min) min=arr[i];
+    // int min=arr[0];
+    // for(int i=1;i<size;i++){
+    //     if(arr[i]<min) min=arr[i];
+    // }
+    int smallest=INT_MAX;
+    int largest=INT_MIN;
+    int minIndex,maxIndex;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]<smallest){
+            smallest=arr[i];
+            minIndex=i;
+        }
     }
-    cout<<"The minimum number is: "<<min;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+            maxIndex=i;
+        }
+    }
+    cout<<"The minimum number is: "<<smallest<<" at index "<<minIndex;
+    cout<<"The maximum number is: "<<largest<<" at index "<<maxIndex;
 }
 
