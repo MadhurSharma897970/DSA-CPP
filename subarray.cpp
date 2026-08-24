4681af851e33e48dbf738559fcef732e59855182
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-24 22:38:36 +0530

    Subarray sum

---
+++ subarray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+
+// int main(){
+//     vector<int>arr={1,2,3,4,5,6}; //possible subarray is (n*(n+1))/2
+//     int n=arr.size();
+//     for(int start=0;start<n;start++){
+//         for(int end=start;end<n;end++){
+//             for(int i=start;i<=end;i++){
+//                 cout<<arr[i];
+//             }
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+int main(){
+    vector<int>arr={3,-4,5,4,-1,7,-8};
+    int n=arr.size();
+    int max_sum=INT_MIN;
+
+    for(int start=0;start<n;start++){
+        int curr_sum=0;
+        for(int end=start;end<n;end++){
+            curr_sum+=arr[end];
+            max_sum=max(max_sum,curr_sum);
+        }
+    }
+
+    cout<<"Maximum subarray sum is: "<<max_sum;
+    
+    return 0;
+}
