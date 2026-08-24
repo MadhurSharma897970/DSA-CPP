a906f92cdfc2c10793001ef282b3b4d5eebb492f
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-24 23:01:58 +0530

    Subarray sum with kadane and brute force

--- subarray.cpp
+++ subarray.cpp
@@ -17,20 +17,36 @@ using namespace std;
 //     }
 // }
 
-int main(){
-    vector<int>arr={3,-4,5,4,-1,7,-8};
-    int n=arr.size();
-    int max_sum=INT_MIN;
+// int main(){
+//     vector<int>arr={3,-4,5,4,-1,7,-8};
+//     int n=arr.size();
+//     int max_sum=INT_MIN;
 
-    for(int start=0;start<n;start++){
-        int curr_sum=0;
-        for(int end=start;end<n;end++){
-            curr_sum+=arr[end];
-            max_sum=max(max_sum,curr_sum);
+//     for(int start=0;start<n;start++){
+//         int curr_sum=0;
+//         for(int end=start;end<n;end++){
+//             curr_sum+=arr[end];
+//             max_sum=max(max_sum,curr_sum);
+//         }
+//     }
+
+//     cout<<"Maximum subarray sum is: "<<max_sum;
+    
+//     return 0;
+// }
+
+//kadane algorithm
+
+int main(){
+    vector<int>arr={2,-1,-3,4,-1,2,1,-5,4};
+    int max_sum=INT_MIN;
+    int curr_sum=0;
+    for(int x:arr){
+        curr_sum+=x;
+        max_sum=max(curr_sum,max_sum);
+        if(curr_sum<0){
+            curr_sum=0;
         }
     }
-
     cout<<"Maximum subarray sum is: "<<max_sum;
-    
-    return 0;
 }
