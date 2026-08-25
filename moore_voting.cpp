e4a7c08ea9a6fb208f2fb55bd5f4e24151b5411a
Madhur Sharma <madhursharmalpu@gmail.com>
2026-08-25 15:05:40 +0530

    Using Hashing

--- moore_voting.cpp
+++ moore_voting.cpp
@@ -1,21 +1,35 @@
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<unordered_map>
+
+// int main(){
+//     vector<int>arr={2, 2, 1, 1, 1, 2, 2};
+//     int n=arr.size();
+//     for(int i=0;i<n;i++){
+//         int count=0;
+//         for(int j=0;j<n;j++){
+//             if(arr[i]==arr[j]){
+//                 count++;
+//             }
+//         }
+//         if(count>n/2){
+//             cout<<"Majority element is: "<<arr[i];
+//             return 0;
+//         }
+//     }
+
+// }
 
 int main(){
     vector<int>arr={2, 2, 1, 1, 1, 2, 2};
     int n=arr.size();
-    for(int i=0;i<n;i++){
-        int count=0;
-        for(int j=0;j<n;j++){
-            if(arr[i]==arr[j]){
-                count++;
-            }
-        }
-        if(count>n/2){
-            cout<<"Majority element is: "<<arr[i];
+    unordered_map<int,int>freq;
+    for(int x:arr){
+        freq[x]++;
+        if(freq[x]>n/2){
+            cout<<"Majority element is: "<<x;
             return 0;
         }
     }
-
 }
